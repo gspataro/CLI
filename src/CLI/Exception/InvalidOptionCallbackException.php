cd8e8838f81176a06afeb2697bca7682a17f183e
Giuseppe Spataro <spatyx@gmail.com>
2022-06-15 16:49:58 +0200

    add(OptionsCollection): add OptionsCollection class
    Add OptionsCollection class to handle options

---
+++ src/CLI/Exception/InvalidOptionCallbackException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GSpataro\CLI\Exception;
+
+use Exception;
+
+class InvalidOptionCallbackException extends Exception
+{
+}
---
+++ src/CLI/OptionsCollection.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace GSpataro\CLI;
+
+final class OptionsCollection
+{
+    /**
+     * Store options
+     *
+     * @var array
+     */
+
+    private array $options = [];
+
+    /**
+     * Verify if the collection has an option
+     *
+     * @param string $tag
+     * @return bool
+     */
+
+    public function has(string $tag): bool
+    {
+        return isset($this->options[$tag]);
+    }
+
+    /**
+     * Add an option to the collection
+     *
+     * @param string $tag
+     * @param array $callback
+     * @param array $args
+     * @param string|null $manpage
+     * @return void
+     */
+
+    public function add(string $tag, array $callback, array $args = [], ?string $manpage = null): void
+    {
+        if ($this->has($tag)) {
+            throw new Exception\OptionFoundException(
+                "An option named '{$tag}' already exists in the collection."
+            );
+        }
+
+        if (
+            !isset($callback[0]) ||
+            !isset($callback[1]) ||
+            !class_exists($callback[0]) ||
+            !method_exists($callback[0], $callback[1])
+        ) {
+            throw new Exception\InvalidOptionCallbackException(
+                "Invalid callback provided to option '{$tag}'. A callback must refer to an existing class and method."
+            );
+        }
+
+        if (!empty($args)) {
+            for ($i = 0; $i < count($args); $i++) {
+                $args[$i]['required'] = $args[$i]['required'] ?? false;
+                $args[$i]['manpage'] = $args[$i]['manpage'] ?? null;
+            }
+        }
+
+        $this->options[$tag] = [
+            "callback" => $callback,
+            "args" => $args,
+            "manpage" => $manpage
+        ];
+    }
+
+    /**
+     * Add multiple options at a time
+     *
+     * @param array $options
+     * @return void
+     */
+
+    public function feed(array $options): void
+    {
+        foreach ($options as $tag => $params) {
+            if (!isset($params['callback'])) {
+                throw new Exception\IncompleteOptionParamsException(
+                    "Incomplete option '{$tag}' definition. An option must include at least a valid callback."
+                );
+            }
+
+            $params['args'] = $params['args'] ?? [];
+            $params['manpage'] = $params['manpage'] ?? null;
+
+            $this->add($tag, $params['callback'], $params['args'], $params['manpage']);
+        }
+    }
+
+    /**
+     * Get an option from the collection
+     *
+     * @param string $tag
+     * @return array
+     */
+
+    public function get(string $tag): array
+    {
+        if (!$this->has($tag)) {
+            throw new Exception\OptionNotFoundException(
+                "Option named '{$tag}' not found."
+            );
+        }
+
+        return $this->options[$tag];
+    }
+
+    /**
+     * Get all the options
+     *
+     * @return array
+     */
+
+    public function getAll(): array
+    {
+        return $this->options;
+    }
+}
