7f2e85c514756687bf9527cd972aef1369e7e536
Giuseppe Spataro <spatyx@gmail.com>
2023-02-28 22:41:50 +0100

    refactor(Output): improve performance and functionality
    Improve performance of format, add striptags to return text without tags and improve printTable functionality

--- src/CLI/Output.php
+++ src/CLI/Output.php
@@ -2,22 +2,24 @@
 
 namespace GSpataro\CLI;
 
+use function PHPSTORM_META\map;
+
 final class Output
 {
     /**
-     * Store regular expressions for ANSI text formatting
+     * Store ANSI codes
      *
      * @var array
      */
 
-    private array $ansiRegex = [
-        "c" => 0, // Clear
-        "b" => 1, // Bold
-        "d" => 2, // Dim
-        "i" => 3, // Italic
-        "u" => 4, // Underline
-        "r" => 31, // Red,
-        "g" => 32 // Green
+    private array $ansiCodes = [
+        "@c;" => "\033[0m", // Clear
+        "@b;" => "\033[1m", // Bold
+        "@d;" => "\033[2m", // Dim
+        "@i;" => "\033[3m", // Italic
+        "@u;" => "\033[4m", // Underline
+        "@r;" => "\033[31m", // Red,
+        "@g;" => "\033[32m" // Green
     ];
 
     /**
@@ -37,19 +39,23 @@ final class Output
 
     private function format(string $text): string
     {
-        $output = str_replace("@nl", "\n", $text);
+        return strtr($text, array_merge(["@nl" => "\n"], $this->ansiCodes));
+    }
 
-        foreach ($this->ansiRegex as $keyword => $code) {
-            $search = "@{$keyword};";
+    /**
+     * Get text without format tags
+     *
+     * @param string $text
+     * @return string
+     */
 
-            if (!str_contains($output, $search)) {
-                continue;
-            }
-
-            $output = str_replace($search, "\033[{$code}m", $output);
-        }
-
-        return $output;
+    private function striptags(string $text): string
+    {
+        return str_replace(
+            array_merge(["@nl"], array_keys($this->ansiCodes)),
+            "",
+            $text
+        );
     }
 
     /**
@@ -79,28 +85,61 @@ final class Output
     /**
      * Print a table to the console
      *
-     * @param array $table
-     * @param int $columns
-     * @param bool $heading
+     * @param array $structure
+     * @param int $columnsNumber
      * @param int $pad
+     * @param array $styles
      * @return void
      */
 
-    public function printTable(array $table, int $columns, bool $heading = true, int $pad = 5): void
+    public function printTable(array $structure, int $columnsNumber, int $pad = 5, array $styles = []): void
     {
+        $styles['heading'] = $styles['heading'] ?? [
+            "prefix" => "\033[1m\033[4m",
+            "suffix" => "\033[0m"
+        ];
+
+        $styles['row'] = $styles['row'] ?? [
+            "prefix" => "",
+            "suffix" => ""
+        ];
+
         $mask = "";
 
-        for ($i = 0; $i < $columns; $i++) {
-            $colSize = max(array_map("strlen", array_column($table, $i)));
-            $colWidth = $colSize + $pad;
+        for ($i = 0; $i < $columnsNumber; $i++) {
+            $colSize = 0;
+
+            foreach ($structure as $row) {
+                $cols = array_values($row)[0] ?? null;
+
+                if (is_null($cols)) {
+                    continue;
+                }
+
+                $colLength = strlen($cols[$i]);
+
+                if ($colLength > $colSize) {
+                    $colSize = $colLength;
+                }
+            }
+
+            $colWidth = $colSize;
+
+            if ($i < $columnsNumber - 1) {
+                $colWidth += $pad;
+            }
+
             $mask .= "%-{$colWidth}.{$colWidth}s";
         }
 
         $mask .= "\n";
-        $headingMask = "\033[1m\033[4m{$mask}\033[0m";
 
-        foreach ($table as $i => $row) {
-            call_user_func_array("printf", array_merge((array) ($i == 0 && $heading ? $headingMask : $mask), $row));
+        foreach ($structure as $i => $row) {
+            $type = array_keys($row)[0] ?? null;
+            $cols = array_values($row)[0] ?? array_fill(0, $columnsNumber, null);
+            $style = $styles[$type] ?? $styles['row'];
+
+            call_user_func_array("printf", array_merge((array) ($style['prefix'] . $mask . $style['suffix']), $cols));
         }
     }
 }
