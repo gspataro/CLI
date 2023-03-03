3e85335fa2655ca0dd8171a1bb93cefb28bb7834
Giuseppe Spataro <spatyx@gmail.com>
2023-03-03 18:45:32 +0100

    update(OutputFormatEnum): extend ansi codes

--- src/CLI/OutputFormatEnum.php
+++ src/CLI/OutputFormatEnum.php
@@ -13,10 +13,20 @@ enum OutputFormatEnum: string
     case dim = "\033[2m";
     case italic = "\033[3m";
     case underline = "\033[4m";
+    case reverse = "\033[7m";
+    case conceal = "\033[8m";
+    case strike = "\033[9m";
+    case double_underline = "\033[21m";
 
     // Colors
+    case gray = "\033[30m";
     case red = "\033[31m";
     case green = "\033[32m";
+    case yellow = "\033[33m";
+    case blue = "\033[34m";
+    case violet = "\033[35m";
+    case lightblue = "\033[36m";
+    case white = "\033[37m";
 
     /**
      * Get cases as array
