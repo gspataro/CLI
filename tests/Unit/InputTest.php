b87610a56b48db8a53b516f575462993afc34d58
Giuseppe Spataro <spatyx@gmail.com>
2023-04-04 19:02:16 +0200

    test(Input): switch to pestphp

---
+++ tests/Unit/InputTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use GSpataro\CLI\Input;
+
+beforeEach(function () {
+    $this->input = new Input([
+        'index.php',
+        'test',
+        'arg1',
+        'arg2'
+    ]);
+});
+
+it('returns the script name', function () {
+    $result = $this->input->getScriptName();
+
+    expect($result)->toBe('index.php');
+});
+
+it('returns the command name', function () {
+    $result = $this->input->getCommandName();
+
+    expect($result)->toBe('test');
+});
+
+it('returns the arguments', function () {
+    $result = $this->input->getArgs();
+
+    expect($result)->toBe([
+        'arg1',
+        'arg2'
+    ]);
+});
