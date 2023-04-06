0b0702feb2dd965e41e66d0d9cb122f89cfacd70
Giuseppe Spataro <spatyx@gmail.com>
2023-04-06 18:02:44 +0200

    test(Stopwatch): add tests for stopwatch helper

---
+++ tests/Unit/StopwatchTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use GSpataro\CLI\Helper\Stopwatch;
+
+it('returns a valid step', function () {
+    $stopwatch = new Stopwatch();
+    $startTime = $stopwatch->start();
+
+    $firstStep = $stopwatch->step();
+    expect($firstStep)
+        ->toBeFloat()
+        ->not->toBe(0.0)
+        ->not->toBe($startTime);
+
+    $secondStep = $stopwatch->step();
+    expect($secondStep)
+        ->toBeFloat()
+        ->not->toBe(0.0)
+        ->not->toBe($startTime)
+        ->not->toBe($firstStep);
+});
+
+it('stops', function () {
+    $stopwatch = new Stopwatch();
+    $startTime = $stopwatch->start();
+
+    $endTime = $stopwatch->stop();
+    expect($endTime)
+        ->toBeFloat()
+        ->not->toBe(0.0)
+        ->not->toBe($startTime);
+
+    $testReset = $stopwatch->step();
+    expect($testReset)
+        ->toBe(0.0);
+});
