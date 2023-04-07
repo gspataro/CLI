6d352c54283b1286a0c3cbe978082ed5907ab552
Giuseppe Spataro <spatyx@gmail.com>
2023-04-07 18:08:53 +0200

    test(table): add tests for each functionality

--- tests/table.php
+++ tests/table.php
@@ -2,12 +2,12 @@
 
 use GSpataro\CLI\Output;
 use GSpataro\CLI\Helper\Table;
+use GSpataro\CLI\Enum\ColorsEnum;
+use GSpataro\CLI\Enum\StylesEnum;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $output = new Output();
-$table = new Table($output);
-
 $structure = [
     ["heading" => ["Name", "Surname", "City"]],
     ["row" => ["Wolfgang Amadeus", "Mozart", "Vienna"]],
@@ -16,6 +16,25 @@ $structure = [
     ["row" => ["Vincenzo", "Bellini", "Catania"]]
 ];
 
-$table->setRows($structure);
+// Test basic functionality
 
-$table->render();
+$basicTable = new Table($output);
+$basicTable->setRows($structure);
+$basicTable->addRow(['lorem', 'ipsum', 'dolor']);
+$basicTable->render();
+
+// Test custom padding
+
+$customPaddingTable = new Table($output);
+$customPaddingTable->setPadding(10);
+$customPaddingTable->setPaddingCharacter('.');
+$customPaddingTable->setRows($structure);
+$customPaddingTable->render();
+
+// Test custom styles
+
+$customStylesTable = new Table($output);
+$customStylesTable->setStyle("row", StylesEnum::bold->value . ColorsEnum::fg_green->value);
+$customStylesTable->setStyle("special", StylesEnum::strike->value);
+$customStylesTable->setRows($structure);
+$customStylesTable->render();
