046ec41cce077160b5fb2f5efce7fa26d333dd99
Giuseppe Spataro <spatyx@gmail.com>
2023-04-11 19:59:53 +0200

    test(TestCase): add private property utilities

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,40 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
+class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Read the content of a private property
+     *
+     * @param object $object
+     * @param string $property
+     * @return mixed
+     */
+
+    public function readPrivateProperty(object $object, string $property): mixed
+    {
+        $reflection = new \ReflectionClass($object);
+        $property = $reflection->getProperty($property);
+        $property->setAccessible(true);
+
+        return $property->getValue($object);
+    }
+
+    /**
+     * Set the content of a private property
+     *
+     * @param object $object
+     * @param string $property
+     * @param mixed $value
+     * @return void
+     */
+
+    public function setPrivateProperty(object $object, string $property, mixed $value): void
+    {
+        $reflection = new \ReflectionClass($object);
+        $property = $reflection->getProperty($property);
+        $property->setAccessible(true);
+
+        $property->setValue($object, $value);
+    }
 }
