c58c1978e8b2d199d33f3d99d7333de86a5018d9
Giuseppe Spataro <spatyx@gmail.com>
2023-04-15 11:34:36 +0200

    update(Command): make use of IO interfaces

--- src/CLI/Command.php
+++ src/CLI/Command.php
@@ -2,12 +2,15 @@
 
 namespace GSpataro\CLI;
 
+use GSpataro\CLI\Interface\InputInterface;
+use GSpataro\CLI\Interface\OutputInterface;
+
 abstract class Command
 {
-    protected readonly Input $input;
-    protected readonly Output $output;
+    protected readonly InputInterface $input;
+    protected readonly OutputInterface $output;
 
-    public function setIO(Input $input, Output $output)
+    public function setIO(InputInterface $input, OutputInterface $output)
     {
         $this->input = $input;
         $this->output = $output;
