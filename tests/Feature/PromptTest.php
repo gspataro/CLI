4f542c3915df5769277250fc42dfa5a074032a5f
Giuseppe Spataro <spatyx@gmail.com>
2023-04-26 18:28:59 +0200

    test(Prompt):  add tests for new confirmation and choice methods

--- tests/Feature/PromptTest.php
+++ tests/Feature/PromptTest.php
@@ -18,20 +18,20 @@ beforeEach(function () {
     $this->prompt = new Prompt($this->input, $this->output);
 });
 
-test('single prompt accepts a user input', function () {
+it('creates a single prompt', function () {
     fwrite($this->stdin, 'foo');
     rewind($this->stdin);
 
-    $result = $this->prompt->single('');
+    $result = $this->prompt->single('Enter something:');
 
     expect($result)->toBe('foo');
 });
 
-test('multiple prompt accepts multiple user inputs', function () {
+it('creates a multiple prompt', function () {
     fwrite($this->stdin, 'foo|bar');
     rewind($this->stdin);
 
-    $result = $this->prompt->multiple('', '|');
+    $result = $this->prompt->multiple('List your interests:', '|');
 
     expect($result)->toBe([
         'foo',
@@ -39,11 +39,62 @@ test('multiple prompt accepts multiple user inputs', function () {
     ]);
 });
 
-test('conceal prompt accepts a user input', function () {
+it('creates a conceal prompt', function () {
     fwrite($this->stdin, 'hidden');
     rewind($this->stdin);
 
-    $result = $this->prompt->conceal('');
+    $result = $this->prompt->conceal('Enter your password:');
 
     expect($result)->toBe('hidden');
+});
+
+it('creates a confirmation prompt that accepts yes', function () {
+    fwrite($this->stdin, 'yes');
+    rewind($this->stdin);
+
+    $result = $this->prompt->confirm('Confirm action?');
+
+    expect($result)->toBeTrue();
+});
+
+it('creates a confirmation prompt that accepts y', function () {
+    fwrite($this->stdin, 'y');
+    rewind($this->stdin);
+
+    $result = $this->prompt->confirm('Confirm action?');
+
+    expect($result)->toBeTrue();
+});
+
+it('creates a confirmation prompt that accepts no', function () {
+    fwrite($this->stdin, 'no');
+    rewind($this->stdin);
+
+    $result = $this->prompt->confirm('Confirm action?');
+
+    expect($result)->toBeFalse();
+});
+
+it('creates a confirmation prompt that accepts n', function () {
+    fwrite($this->stdin, 'n');
+    rewind($this->stdin);
+
+    $result = $this->prompt->confirm('Confirm action?');
+
+    expect($result)->toBeFalse();
+});
+
+it('creates a choice prompt', function () {
+    fwrite($this->stdin, 1);
+    rewind($this->stdin);
+
+    $choices = [
+        'Mozart',
+        'Beethoven',
+        'Rachmaninoff'
+    ];
+
+    $result = $this->prompt->choice('Choose your favourite composer:', $choices);
+
+    expect($choices[$result])->toBe('Beethoven');
 });
