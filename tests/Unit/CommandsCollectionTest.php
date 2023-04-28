4dbf2287b431dea1f766165e018fd8d5d186ddfd
Giuseppe Spataro <spatyx@gmail.com>
2023-04-28 18:28:30 +0200

    test(CommandsCollection): remove unused tests

--- tests/Unit/CommandsCollectionTest.php
+++ tests/Unit/CommandsCollectionTest.php
@@ -34,26 +34,11 @@ it('adds a command', function () {
     ]);
 });
 
-it('throw an exception with an invalid command callback class', function () {
-    $this->commandsCollection->add('test', ['invalid', 'method'], []);
-})->throws(
-    InvalidCommandCallbackException::class,
-    "Invalid callback for command 'test'. The first element of the array must be an object."
-);
-
 it('throws an exception with an invalid command callback class (not extending base command)', function () {
     $this->commandsCollection->add('test', [new \Tests\Utilities\InvalidController(), 'method'], []);
 })->throws(
     InvalidCommandCallbackException::class,
     "Invalid callback for command 'test'. The command object must extend the GSpataro\\CLI\\Helper\\BaseCommand class"
-);
-
-it('throws an exception with an invalid command callback method', function () {
-    $this->commandsCollection->add('test', [new \Tests\Utilities\Controller(), 'nonexisting'], []);
-})->throws(
-    InvalidCommandCallbackException::class,
-    "Invalid callback for command 'test'. " .
-    "The second element of the array must be a method of 'Tests\Utilities\Controller'."
 );
 
 it('throws an exception if a command already exists', function () {
