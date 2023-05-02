4bc53160c857e1484e33cff2ca3c4e953d359909
Giuseppe Spataro <spatyx@gmail.com>
2023-05-02 19:35:48 +0200

    test: add output buffer methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,22 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 class TestCase extends BaseTestCase
 {
     /**
+     * Store output buffering status
+     *
+     * @var bool
+     */
+
+    private bool $outputBufferActive = false;
+
+    /**
+     * Store output buffering level
+     *
+     * @var int
+     */
+
+    private int $outputBufferLevel;
+
+    /**
      * Read the content of a private property
      *
      * @param object $object
@@ -39,5 +55,50 @@ class TestCase extends BaseTestCase
         $property->setAccessible(true);
 
         $property->setValue($object, $value);
+    }
+
+    /**
+     * Start output buffer
+     *
+     * @return void
+     */
+
+    public function startOutputBuffer(): void
+    {
+        ob_start();
+        $this->outputBufferActive = true;
+        $this->outputBufferLevel = ob_get_level();
+    }
+
+    /**
+     * Get output
+     *
+     * @return string|false
+     */
+
+    public function getOutput(): string|false
+    {
+        return ob_get_clean();
+    }
+
+    /**
+     * End output buffer
+     *
+     * @return void
+     */
+
+    public function endOutputBuffer(): void
+    {
+        if (!$this->outputBufferActive) {
+            return;
+        }
+
+        if ($this->outputBufferLevel !== ob_get_level()) {
+            while (ob_get_level() >= $this->outputBufferLevel) {
+                ob_end_clean();
+            }
+        }
+
+        $this->outputBufferActive = false;
     }
 }
