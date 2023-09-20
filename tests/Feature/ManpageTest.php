408acc17310b75999ff623baff2c246e16955c9f
Giuseppe Spataro <spatyx@gmail.com>
2023-09-20 22:33:36 +0200

    test(ManpageTest): make use of new output stream functionality

--- tests/Feature/ManpageTest.php
+++ tests/Feature/ManpageTest.php
@@ -8,11 +8,12 @@ use GSpataro\CLI\Helper\Manpage;
 uses(\Tests\TestCase::class)->group('helpers');
 
 beforeEach(function () {
+    $this->outputStream = fopen('gstest://output', 'r+');
     $this->input = new Input([
         'script.php',
         'help'
     ]);
-    $this->output = new Output();
+    $this->output = new Output($this->outputStream);
     $this->commands = new CommandsCollection();
 
     $this->commands->create('login')
@@ -56,40 +57,52 @@ beforeEach(function () {
 
 it('prints an help table', function () {
     $this->manpage->render();
-    $table = $this->readPrivateProperty($this->manpage, 'table');
-    $result = $table->build();
 
-    $expected = '{bold}login             {clear}{bold}Sign in{clear}{nl}';
-    $expected .= '{italic}-e, -email        {clear}{italic}Your email address (required){clear}{nl}';
-    $expected .= '{italic}-p, -password     {clear}{italic}Your password (required){clear}{nl}';
-    $expected .= '{nl}';
-    $expected .= '{bold}register          {clear}{bold}Sign up{clear}{nl}';
-    $expected .= '{italic}-e, -email        {clear}{italic}Your email address (required){clear}{nl}';
-    $expected .= '{italic}-p, -password     {clear}{italic}Choose a password (required){clear}{nl}';
-    $expected .= '{italic}--name            {clear}{italic}Your full name{clear}{nl}';
-    $expected .= '{nl}';
-    $expected .= '{bold}help              {clear}{bold}List available commands{clear}{nl}';
+    rewind($this->outputStream);
+    $result = stream_get_contents($this->outputStream);
+
+    $expected = "Usage: script.php \033[1m\033[4mcommand\033[0m \033[3moption\n\033[0m\n";
+    $expected .= "Available commands\033[0m\n";
+    $expected .= "\033[1mlogin             \033[0m\033[1mSign in\033[0m\n";
+    $expected .= "\033[3m-e, -email        \033[0m\033[3mYour email address (required)\033[0m\n";
+    $expected .= "\033[3m-p, -password     \033[0m\033[3mYour password (required)\033[0m\n";
+    $expected .= "\n";
+    $expected .= "\033[1mregister          \033[0m\033[1mSign up\033[0m\n";
+    $expected .= "\033[3m-e, -email        \033[0m\033[3mYour email address (required)\033[0m\n";
+    $expected .= "\033[3m-p, -password     \033[0m\033[3mChoose a password (required)\033[0m\n";
+    $expected .= "\033[3m--name            \033[0m\033[3mYour full name\033[0m\n";
+    $expected .= "\n";
+    $expected .= "\033[1mhelp              \033[0m\033[1mList available commands\033[0m\n";
+    $expected .= "\033[0m\n";
 
     expect($result)->toBe($expected);
 });
 
 it('can be translated', function () {
+    $this->manpage->setLocale('usage', 'Utilizzo');
+    $this->manpage->setLocale('command', 'comando');
+    $this->manpage->setLocale('option', 'opzione');
+    $this->manpage->setLocale('available_commands', 'Comandi disponibili');
     $this->manpage->setLocale('required', 'richiesto');
     $this->manpage->setLocale('list_available_commands', 'Mostra comandi disponibili');
     $this->manpage->render();
-    $table = $this->readPrivateProperty($this->manpage, 'table');
-    $result = $table->build();
 
-    $expected = '{bold}login             {clear}{bold}Sign in{clear}{nl}';
-    $expected .= '{italic}-e, -email        {clear}{italic}Your email address (richiesto){clear}{nl}';
-    $expected .= '{italic}-p, -password     {clear}{italic}Your password (richiesto){clear}{nl}';
-    $expected .= '{nl}';
-    $expected .= '{bold}register          {clear}{bold}Sign up{clear}{nl}';
-    $expected .= '{italic}-e, -email        {clear}{italic}Your email address (richiesto){clear}{nl}';
-    $expected .= '{italic}-p, -password     {clear}{italic}Choose a password (richiesto){clear}{nl}';
-    $expected .= '{italic}--name            {clear}{italic}Your full name{clear}{nl}';
-    $expected .= '{nl}';
-    $expected .= '{bold}help              {clear}{bold}Mostra comandi disponibili{clear}{nl}';
+    rewind($this->outputStream);
+    $result = stream_get_contents($this->outputStream);
+
+    $expected = "Usage: script.php \033[1m\033[4mcomando\033[0m \033[3mopzione\n\033[0m\n";
+    $expected .= "Comandi disponibili\033[0m\n";
+    $expected .= "\033[1mlogin             \033[0m\033[1mSign in\033[0m\n";
+    $expected .= "\033[3m-e, -email        \033[0m\033[3mYour email address (richiesto)\033[0m\n";
+    $expected .= "\033[3m-p, -password     \033[0m\033[3mYour password (richiesto)\033[0m\n";
+    $expected .= "\n";
+    $expected .= "\033[1mregister          \033[0m\033[1mSign up\033[0m\n";
+    $expected .= "\033[3m-e, -email        \033[0m\033[3mYour email address (richiesto)\033[0m\n";
+    $expected .= "\033[3m-p, -password     \033[0m\033[3mChoose a password (richiesto)\033[0m\n";
+    $expected .= "\033[3m--name            \033[0m\033[3mYour full name\033[0m\n";
+    $expected .= "\n";
+    $expected .= "\033[1mhelp              \033[0m\033[1mMostra comandi disponibili\033[0m\n";
+    $expected .= "\033[0m\n";
 
     expect($result)->toBe($expected);
 });
